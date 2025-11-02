643cffd12cd6a8c634bf9bf34ca88d1078c5be84
yocefecoy <szdfffffxhjgs@gmail.com>
2025-11-03 00:27:05 +0200

    Updated

--- src/ArrayListSum.java
+++ src/ArrayListSum.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 
 public class ArrayListSum {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
         numbers.add(2);
-        numbers.add(3);
         numbers.add(4);
-        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(9);
 
-        int sum = 0;
-        for (int num : numbers) {
-            sum += num;
+        int sum=0;
+        for (int num : numbers){
+            sum +=num;
         }
-
-        System.out.println("Sum of ArrayList elements: " + sum);
+        System.out.println("sum= "+sum);
     }
+
 }
